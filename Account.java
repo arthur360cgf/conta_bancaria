7295d0603e067a43397bc5703c28a936297f0650
arthur360cgf <arthurmattei115@gmail.com>
2022-01-20 11:57:29 -0300

    add method get and set

--- Account.java
+++ Account.java
@@ -1,25 +1,60 @@
+import java.util.Scanner;
 public class Account {
   private String name;
-  private float value;
+  private double value;
   private int numAccount;
-  private int withdraw;
+  private double withdraw;
   
-  public Account(String name, float value, int numAccount, int withdraw){
+  Scanner sc = new Scanner(System.in);
+
+  public Account(String name, double value, int numAccount, double withdraw){
     this.name = name;
     this.value = value;
     this.numAccount = numAccount;
     this.withdraw = withdraw;
 
   }
-  
+  public String getName(){
+    return name;
+  }
+  public void setName(String name){
+    this.name = name;
+  }
+  public double getValue(){
+    return value;
+  }
+  public void setValue(double value){
+    this.value = value;
+  }
+  public int getNumAccount(){
+    return numAccount;
+  }
+  public void setNumAccount(int numAccount){
+    this.numAccount = numAccount;
+  }
+  public double getWithdraw(){
+    return withdraw;
+  }
+  public void setWithdraw(double withdraw){
+    this.withdraw = withdraw;
+  }
   void statement(){
     System.out.println("Name:"+name+"/n"+"Value in account:"+value+"n/"+"Account number:"+numAccount);
   }
-  public float Money(int withdraw){
-    float y = value - withdraw;
-
+  public double Money(double withdraw){
+    double y = value - withdraw;
+    String answer;
+    System.out.println("How many money you wanna drew?");
+    withdraw = sc.nextDouble();
     if (value >= withdraw){
-      value = y;
+      while(answer == "Y"){
+        value = y;
+        System.out.println("You drew R$"+withdraw);
+        System.out.println("Wanna make another withdraw? Y/N");
+        answer = sc.next();
+        return y;
+      }
+      return y;
 
     }
 
