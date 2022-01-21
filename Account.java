9f1fa15575911e81b320424e69334700171cad17
arthur360cgf <arthurmattei115@gmail.com>
2022-01-21 12:07:19 -0300

    continue

--- Account.java
+++ Account.java
@@ -4,11 +4,13 @@ public class Account {
   private double value;
   private int numAccount;
   private double withdraw;
+  private double balance;
   
   Scanner sc = new Scanner(System.in);
 
-  public Account(String name, double value, int numAccount, double withdraw){
+  public Account(String name, double balance, double value, int numAccount, double withdraw){
     this.name = name;
+    this.balance = balance;
     this.value = value;
     this.numAccount = numAccount;
     this.withdraw = withdraw;
@@ -20,6 +22,14 @@ public class Account {
   public void setName(String name){
     this.name = name;
   }
+  public double getBalance(){
+    return balance;
+  }
+
+  public void setBalance(double balance){
+    this.balance = balance;
+  }
+
   public double getValue(){
     return value;
   }
@@ -39,26 +49,81 @@ public class Account {
     this.withdraw = withdraw;
   }
   void statement(){
-    System.out.println("Name:"+name+"/n"+"Value in account:"+value+"n/"+"Account number:"+numAccount);
+    System.out.println("Name:"+name+"\n"+"Value in account:"+value+"\n"+"Account number:"+numAccount);
   }
-  public double Money(double withdraw){
-    double y = value - withdraw;
-    String answer;
+
+  public void extract(){
+    System.out.println("\tEXTRACT");
+    System.out.println("Name: "+ this.name);
+    System.out.println("Account number: "+this.numAccount);
+    System.out.println("teh balance in your account is: R$%.2f\n"+this.balance);
+    System.out.println("Today you make a total of "+this.withdraw+" drews");
+  }
+
+  public void Money(double value){
+  
     System.out.println("How many money you wanna drew?");
     withdraw = sc.nextDouble();
-    if (value >= withdraw){
-      while(answer == "Y"){
-        value = y;
-        System.out.println("You drew R$"+withdraw);
-        System.out.println("Wanna make another withdraw? Y/N");
-        answer = sc.next();
-        return y;
+    if (balance >= value){
+      balance -= value;
+      withdraw++;        
+        System.out.println("You drew R$"+value);
+        System.out.println("New balance is: "+balance+"\n");
+      
       }
-      return y;
+      else{
+        System.out.println("Its not possible make this operation because tha value of you account are less than you have in your account. Make a deposit or drew a smaler value");
+       
 
-    }
-
-    return y;
+      } 
 
   }
+
+  public void deposit(double value){
+    balance += value;
+    System.out.println("How much you want to deposit?");
+    value = sc.nextDouble();
+    System.out.println("You make a deposit in a value of: R$"+value);
+
+  }
+
+  public void iniciate(){
+    int option;
+    do{
+      showMenu();
+      option = sc.nextInt();
+      chooseOption(option);
+    }while(option != 4);
+  }
+
+  public void showMenu(){
+    System.out.println("\t Choose the option you want"); 
+    System.out.println("1 - Consult Extract");
+    System.out.println("2 - Withdraw"); 
+    System.out.println("3 - Deposit");
+    System.out.println("4 - Exit \n"); 
+    System.out.println("Option: ");
+    
+  }
+
+  public void chooseOption(int option){
+    double value;
+
+    switch(option){
+      case 1:
+        extract();
+        break;
+      case 2:
+        if(withdraw < 3){
+          System.out.println("How much you want drew: ");
+          value = sc.nextDouble();
+          Money(value);
+        }
+        else{
+          System.out.println("You already reached your daily limit");
+        }
+
+    }
+  }
+
 }
